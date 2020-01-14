5d01c7382ea250827eb38ec9c7a5783f21fa402b
MikeTangoEcho <mathieu.thine@gmail.com>
2020-01-14 15:52:55 +0100

    Update release.sh
    * Add sha256

--- cmd/freegeoip/release.sh
+++ cmd/freegeoip/release.sh
@@ -3,11 +3,12 @@
 VERSION=$(go run main.go -version | cut -d\  -f2)
 
 function pack() {
-	dir=freegeoip-$VERSION-$1
+	dir=freegeoip-v$VERSION-$1
 	mkdir $dir
 	cp -r ${binary} public $dir
 	sync
-	gtar --owner=0 --group=0 -czf ${dir}.tar.gz $dir
+	tar -czf ${dir}.tar.gz $dir
+	sha256sum ${dir}.tar.gz | cut -f 1 -d " " > ${dir}.tar.gz.sha256
 	rm -rf $dir
 }
 
